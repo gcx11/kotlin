c23420126457fc42a1695a8b282bede023f026e9
Ilya Kirillov <darthorimar@gmail.com>
2019-03-23 22:07:56 +0300

    Fix old j2k build

--- j2k/build.gradle.kts
+++ j2k/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     testRuntime(intellijDep())
 
     compile(kotlinStdlib())
+    compile(project(":idea:idea-core"))
     compile(project(":compiler:frontend"))
     compile(project(":compiler:frontend.java"))
     compile(project(":compiler:light-classes"))
